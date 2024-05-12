1379a84e1f2462d4bf37cb0a0231187ef1d66ec9
Tarun Tailor <taruntailor7@gmail.com>
2024-05-12 23:46:48 +0530

    message emitted on server from client side

--- chat-app/apps/web/app/page.tsx
+++ chat-app/apps/web/app/page.tsx
@@ -1,7 +1,31 @@
 "use client"
 
+import { useState } from "react";
+import classes from "./page.module.css"
+import { useSocket } from "../context/SocketProvider";
+
 export default function Page() {
+  const { sendMessage } = useSocket();
+  const [message, setMessage] = useState("");
+
   return (
-    <h1>hello</h1>
+    <div>
+      <div>
+        <h1>All Messages will Appear here</h1>
+      </div>
+      <div>
+        <input 
+          onChange={(e) => setMessage(e.target.value)}
+          className={classes['chat-input']} 
+          placeholder="Message..." 
+        />
+        <button 
+          onClick={(e) => sendMessage(message)}
+          className={classes['button']}
+        >
+          Send
+        </button>
+      </div>  
+    </div>
   )
 }
--- chat-app/apps/web/context/SocketProvider.tsx
+++ chat-app/apps/web/context/SocketProvider.tsx
@@ -1,7 +1,7 @@
 "use client"
 
-import React, { useCallback, useContext, useEffect } from "react";
-import { io } from "socket.io-client";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { Socket, io } from "socket.io-client";
 
 interface SocketProviderProps {
     children?: React.ReactNode;
@@ -23,15 +23,21 @@ export const useSocket = () => {
 }
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({children}) => {
+    const [socket, setSocket] = useState<Socket>();
+
     const sendMessage: ISocketContext['sendMessage'] = useCallback((msg) => {
         console.log("Send Message", msg); 
-    }, []);
+        if (socket) {
+            socket.emit('event:message', { message: msg });
+        }
+    }, [socket]);
 
     useEffect(() => {
         const _socket = io('http://localhost:8000');
-
+        setSocket(_socket);
         return () => {
             _socket.disconnect();
+            setSocket(undefined)
         };
     }, []);
 
