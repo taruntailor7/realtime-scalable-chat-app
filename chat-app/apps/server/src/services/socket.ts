e8376db8b4d3d802bb5986cfcec5f724f5169cef
Tarun Tailor <taruntailor7@gmail.com>
2024-05-12 22:30:33 +0530

    initListeners function added in class for emitting events

--- chat-app/apps/server/src/services/socket.ts
+++ chat-app/apps/server/src/services/socket.ts
@@ -8,6 +8,19 @@ class SocketService {
         this._io = new Server();
     }
 
+    public initListeners() {
+        const io = this._io;
+        console.log("Init Socket Listeners..."); 
+        
+        io.on("connect", (socket) => {
+            console.log(`New Socket Connected`, socket.id); 
+
+            socket.on("event:message", async ({ message }: {message: string}) => {
+                console.log("New Message Received", message);
+            });
+        });
+    }
+
     get io() {
         return this._io;
     }
