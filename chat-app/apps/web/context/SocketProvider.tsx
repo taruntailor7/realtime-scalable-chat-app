03a0875566bd56e13b9b325d8a3c7cb34a8c2c27
Tarun Tailor <taruntailor7@gmail.com>
2024-05-12 23:18:12 +0530

    custom hook useSocket created

--- chat-app/apps/web/context/SocketProvider.tsx
+++ chat-app/apps/web/context/SocketProvider.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useContext, useEffect } from "react";
 import { io } from "socket.io-client";
 
 interface SocketProviderProps {
@@ -12,6 +12,15 @@ interface ISocketContext{
 }
 
 const SocketContext = React.createContext<ISocketContext | null>(null);
+
+export const useSocket = () => {
+    const state = useContext(SocketContext);
+
+    if (!state) throw new Error(`state is undefined`);
+
+    return state;
+
+}
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({children}) => {
     const sendMessage: ISocketContext['sendMessage'] = useCallback((msg) => {
